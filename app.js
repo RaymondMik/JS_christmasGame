a9ecc493d2d4bd104ba71ee1223ea8d74bf4994e
Ramón Miklus <ramonpersonal@Ramons-MacBook-Pro.local>
2016-12-22 20:45:06 +0100

    Mobile version

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// check screen size and orientation
-var screenOrientation = window.matchMedia("(orientation: portrait)");
-
 // setup
 var options = {
     totalTime: 60,
@@ -41,110 +38,111 @@ time.innerHTML = this.options.totalTime;
 var audioWin = new Audio("assets/dog_barking.mp3");
 var audioLoose = new Audio("assets/dog_crying.mp3");
 var audioSigletta = new Audio("assets/jingle_bells.mp3");
+var puppyWidth = screen.width < 400 ? 50 : 100;
+var puppyHeight = screen.width < 400 ? 50 : 100;
+var puppyTopPos = screen.width < 400 ? 125 : 500;
+var puppyLeftPos = screen.width < 400 ? 125 : 500;
 audioSigletta.loop = true;
 
-if ( screen.width < 767 ) {
-    var message = '<h2>This game is available for tablets and computers only</h2>';
-    this.triggerModal(message, false);
-} else {
 
-    generateFaces();
-    var startTimer = setInterval(timer, 1000);
-    this.audioSigletta.play();
+generateFaces();
+var startTimer = setInterval(timer, 1000);
+//this.audioSigletta.play();
 
-    function timer() {
-        time.innerHTML = this.options.totalTime;
-        if (this.options.totalTime >= 0) {
-           this.options.totalTime -= 1; 
-        } 
-        if (this.options.totalTime === -1) {
-            this.gameOver();
-            var message = '<h1>Game Over</h1><h3>Congratulations!<br>Level: ' + currentLevel + '<br> Points: ' + currentPoints + '</h3>';
-            self.triggerModal(message, true);
-        }
+function timer() {
+    time.innerHTML = this.options.totalTime;
+    if (this.options.totalTime >= 0) {
+       this.options.totalTime -= 1; 
+    } 
+    if (this.options.totalTime === -1) {
+        this.gameOver();
+        var message = '<h1>Game Over</h1><h3>Congratulations!<br>Level: ' + currentLevel + '<br> Points: ' + currentPoints + '</h3>';
+        self.triggerModal(message, true);
+    }
+}
+
+// generate faces
+function generateFaces() {
+    var self = this;
+   
+    for ( var i = 0; i < this.options.numberOfFaces; i++) {
+        //generate, place and style puppy face
+        var puppyFace = document.createElement('img');
+        puppyFace.className = 'item';
+
+        var randomPositionTop = Math.floor(Math.random() * puppyTopPos);
+        var randomPositionLeft = Math.floor(Math.random() * puppyLeftPos);
+        puppyFace.style.top = randomPositionTop + 'px';
+        puppyFace.style.left = randomPositionLeft + 'px';
+        puppyFace.style.width = puppyWidth + 'px';
+        puppyFace.style.height = puppyHeight + 'px';
+        puppyFace.src = 'assets/tommy.png';
+        //add images to the left side
+        theLeftSide.appendChild(puppyFace);
+        //clone leftSide node
+        var leftSideImages = theLeftSide.cloneNode(true);
+        leftSideImages.id = "new-puppy-face";
+        //delete last child of LeftSideImages
+        leftSideImages.removeChild(leftSideImages.lastChild);
+        //add leftSideImages to the rightSide div
+        theRightSide.appendChild(leftSideImages);
     }
 
-    // generate faces
-    function generateFaces() {
-        var self = this;
-       
-        for ( var i = 0; i < this.options.numberOfFaces; i++) {
-            //generate, place and style puppy face
-            var puppyFace = document.createElement('img');
-            puppyFace.className = 'item';
-            var randomPositionTop = Math.floor(Math.random() * 501);
-            var randomPositionLeft = Math.floor(Math.random() * 501);
-            puppyFace.style.top = randomPositionTop + 'px';
-            puppyFace.style.left = randomPositionLeft + 'px';
-            puppyFace.style.width = 100 + 'px';
-            puppyFace.style.height = 100 + 'px';
-            puppyFace.src = 'assets/tommy.png';
-            //add images to the left side
-            theLeftSide.appendChild(puppyFace);
-            //clone leftSide node
-            var leftSideImages = theLeftSide.cloneNode(true);
-            leftSideImages.id = "new-puppy-face";
-            //delete last child of LeftSideImages
-            leftSideImages.removeChild(leftSideImages.lastChild);
-            //add leftSideImages to the rightSide div
-            theRightSide.appendChild(leftSideImages);
+    // add onclick event to last child of left side
+    theLeftSide.lastChild.onclick = function nextLevel(event) {
+        event.stopPropagation();
+        audioWin.play();
+
+        //remove images
+        while (theLeftSide.firstChild && theRightSide.firstChild) {
+            theLeftSide.removeChild(theLeftSide.firstChild);
+            theRightSide.removeChild(theRightSide.firstChild);
         }
+        self.options.numberOfFaces += self.options.increaseFaces;
+        levelCount++;
+        currentPoints += self.options.guessPoints;
+        points.innerHTML = currentPoints;
 
-        // add onclick event to last child of left side
-        theLeftSide.lastChild.onclick = function nextLevel(event) {
-            event.stopPropagation();
-            audioWin.play();
-
-            //remove images
-            while (theLeftSide.firstChild && theRightSide.firstChild) {
-                theLeftSide.removeChild(theLeftSide.firstChild);
-                theRightSide.removeChild(theRightSide.firstChild);
-            }
-            self.options.numberOfFaces += self.options.increaseFaces;
-            levelCount++;
-            currentPoints += self.options.guessPoints;
+        //generate a new set of Faces
+        generateFaces();
+        //if three guesses go to next level
+        if (levelCount % 3 == 0) {
+            currentLevel += 1;
+            currentPoints += self.options.levelPoints;
             points.innerHTML = currentPoints;
-
-            //generate a new set of Faces
-            generateFaces();
-            //if three guesses go to next level
-            if (levelCount % 3 == 0) {
-                currentLevel += 1;
-                currentPoints += self.options.levelPoints;
-                points.innerHTML = currentPoints;
-                level.innerHTML = currentLevel;
-            }
-        }
-
-    }
-
-    // when the player clicks on anything except the correct face
-    theBody.onclick = function() {
-        count++;
-        if (count < 3) {
-            var puppy = lives.getElementsByTagName('img');
-            lives.removeChild(puppy[0]);
-            audioLoose.play();
-            if (currentPoints > 100) {
-                currentPoints -= this.options.losePoints;
-            }
-        }
-        if (count === 3) {
-            self.gameOver();
-            var message = '<h1>Game Over</h1><h3>Congratulations!<br>Level: ' + currentLevel + '<br> Points: ' + currentPoints + '</h3>';
-            self.triggerModal(message, true);
-        }
-    }
-
-    audioButton.onclick = function() {
-        if (audioSigletta.paused) {
-            audioSigletta.play();
-            audioIcon.style.backgroundImage = 'url("assets/music_yes.png")';
-        } else {
-            audioSigletta.pause();
-            audioIcon.style.backgroundImage = 'url("assets/music_no.png")';
+            level.innerHTML = currentLevel;
         }
     }
 
 }
+
+// when the player clicks on anything except the correct face
+theBody.onclick = function() {
+    count++;
+    if (count < 3) {
+        var puppy = lives.getElementsByTagName('img');
+        lives.removeChild(puppy[0]);
+        audioLoose.play();
+        if (currentPoints > 100) {
+            currentPoints -= this.options.losePoints;
+        }
+    }
+    if (count === 3) {
+        self.gameOver();
+        var message = '<h1>Game Over</h1><h3>Congratulations!<br>Level: ' + currentLevel + '<br> Points: ' + currentPoints + '</h3>';
+        self.triggerModal(message, true);
+    }
+}
+
+audioButton.onclick = function() {
+    if (audioSigletta.paused) {
+        audioSigletta.play();
+        audioIcon.style.backgroundImage = 'url("assets/music_yes.png")';
+    } else {
+        audioSigletta.pause();
+        audioIcon.style.backgroundImage = 'url("assets/music_no.png")';
+    }
+}
+
+
 
